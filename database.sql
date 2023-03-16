2d2c1a2f7dca5e966ce3131f762cad23b90af6f4
Pablo <117945241+kaare2912@users.noreply.github.com>
2023-03-16 19:25:04 +0100

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,377 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Vært: 127.0.0.1
+-- Genereringstid: 10. 11 2022 kl. 21:10:04
+-- Serverversion: 10.4.25-MariaDB
+-- PHP-version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+CREATE DATABASE IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `database`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `phone_app_chat`
+--
+
+DROP TABLE IF EXISTS `phone_app_chat`;
+CREATE TABLE `phone_app_chat` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `channel` varchar(20) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `phone_calls`
+--
+
+DROP TABLE IF EXISTS `phone_calls`;
+CREATE TABLE `phone_calls` (
+  `id` int(11) NOT NULL,
+  `owner` varchar(20) NOT NULL COMMENT 'Num tel proprio',
+  `num` varchar(20) NOT NULL COMMENT 'Num reférence du contact',
+  `incoming` int(11) NOT NULL COMMENT 'Défini si on est à l''origine de l''appels',
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `accepts` int(11) NOT NULL COMMENT 'Appels accepter ou pas'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `phone_messages`
+--
+
+DROP TABLE IF EXISTS `phone_messages`;
+CREATE TABLE `phone_messages` (
+  `id` int(11) NOT NULL,
+  `transmitter` varchar(20) NOT NULL,
+  `receiver` varchar(20) NOT NULL,
+  `message` varchar(1000) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isRead` int(11) NOT NULL DEFAULT 0,
+  `owner` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `phone_twitter`
+--
+
+DROP TABLE IF EXISTS `phone_twitter`;
+CREATE TABLE `phone_twitter` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `message` varchar(300) NOT NULL,
+  `time` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_srv_data`
+--
+
+DROP TABLE IF EXISTS `vrp_srv_data`;
+CREATE TABLE `vrp_srv_data` (
+  `dkey` varchar(255) NOT NULL,
+  `dvalue` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_users`
+--
+
+DROP TABLE IF EXISTS `vrp_users`;
+CREATE TABLE `vrp_users` (
+  `id` int(11) NOT NULL,
+  `last_login` varchar(255) DEFAULT NULL,
+  `last_date` varchar(255) NOT NULL DEFAULT '',
+  `whitelisted` tinyint(1) DEFAULT NULL,
+  `banned` tinyint(1) DEFAULT NULL,
+  `DmvTest` int(11) NOT NULL DEFAULT 0,
+  `warnings` int(11) NOT NULL DEFAULT 0,
+  `ban_reason` varchar(250) DEFAULT NULL,
+  `discord` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_business`
+--
+
+DROP TABLE IF EXISTS `vrp_user_business`;
+CREATE TABLE `vrp_user_business` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `capital` int(11) DEFAULT NULL,
+  `laundered` int(11) DEFAULT NULL,
+  `reset_timestamp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_data`
+--
+
+DROP TABLE IF EXISTS `vrp_user_data`;
+CREATE TABLE `vrp_user_data` (
+  `user_id` int(11) NOT NULL,
+  `dkey` varchar(255) NOT NULL,
+  `dvalue` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_homes`
+--
+
+DROP TABLE IF EXISTS `vrp_user_homes`;
+CREATE TABLE `vrp_user_homes` (
+  `user_id` int(11) NOT NULL,
+  `home` varchar(255) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_identities`
+--
+
+DROP TABLE IF EXISTS `vrp_user_identities`;
+CREATE TABLE `vrp_user_identities` (
+  `user_id` int(11) NOT NULL,
+  `registration` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_ids`
+--
+
+DROP TABLE IF EXISTS `vrp_user_ids`;
+CREATE TABLE `vrp_user_ids` (
+  `identifier` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_moneys`
+--
+
+DROP TABLE IF EXISTS `vrp_user_moneys`;
+CREATE TABLE `vrp_user_moneys` (
+  `user_id` int(11) NOT NULL,
+  `wallet` int(11) DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `debt` int(11) DEFAULT 0,
+  `depositOnLogin` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_user_vehicles`
+--
+
+DROP TABLE IF EXISTS `vrp_user_vehicles`;
+CREATE TABLE `vrp_user_vehicles` (
+  `user_id` int(11) NOT NULL,
+  `vehicle` varchar(100) NOT NULL,
+  `veh_type` varchar(255) NOT NULL DEFAULT 'default',
+  `vehicle_plate` varchar(255) NOT NULL,
+  `impound` int(11) NOT NULL DEFAULT 0,
+  `hashkey` varchar(255) DEFAULT NULL,
+  `vehicle_colorprimary` varchar(255) DEFAULT NULL,
+  `modifications` mediumtext NOT NULL,
+  `vehicle_colorsecondary` varchar(255) DEFAULT NULL,
+  `vehicle_fuel` varchar(50) DEFAULT '60.0',
+  `vehicle_damage` varchar(50) DEFAULT '1000.0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `vrp_wanted`
+--
+
+DROP TABLE IF EXISTS `vrp_wanted`;
+CREATE TABLE `vrp_wanted` (
+  `user_id` int(11) DEFAULT NULL,
+  `wantedreason` varchar(100) DEFAULT NULL,
+  `wantedby` int(11) DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Begrænsninger for dumpede tabeller
+--
+
+--
+-- Indeks for tabel `phone_calls`
+--
+ALTER TABLE `phone_calls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks for tabel `phone_messages`
+--
+ALTER TABLE `phone_messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks for tabel `phone_twitter`
+--
+ALTER TABLE `phone_twitter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks for tabel `vrp_srv_data`
+--
+ALTER TABLE `vrp_srv_data`
+  ADD PRIMARY KEY (`dkey`);
+
+--
+-- Indeks for tabel `vrp_users`
+--
+ALTER TABLE `vrp_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks for tabel `vrp_user_business`
+--
+ALTER TABLE `vrp_user_business`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeks for tabel `vrp_user_data`
+--
+ALTER TABLE `vrp_user_data`
+  ADD PRIMARY KEY (`user_id`,`dkey`);
+
+--
+-- Indeks for tabel `vrp_user_homes`
+--
+ALTER TABLE `vrp_user_homes`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeks for tabel `vrp_user_ids`
+--
+ALTER TABLE `vrp_user_ids`
+  ADD PRIMARY KEY (`identifier`),
+  ADD KEY `fk_user_ids_users` (`user_id`);
+
+--
+-- Indeks for tabel `vrp_user_moneys`
+--
+ALTER TABLE `vrp_user_moneys`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeks for tabel `vrp_user_vehicles`
+--
+ALTER TABLE `vrp_user_vehicles`
+  ADD PRIMARY KEY (`user_id`,`vehicle`);
+
+--
+-- Brug ikke AUTO_INCREMENT for slettede tabeller
+--
+
+--
+-- Tilføj AUTO_INCREMENT i tabel `phone_calls`
+--
+ALTER TABLE `phone_calls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tilføj AUTO_INCREMENT i tabel `phone_messages`
+--
+ALTER TABLE `phone_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tilføj AUTO_INCREMENT i tabel `phone_twitter`
+--
+ALTER TABLE `phone_twitter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tilføj AUTO_INCREMENT i tabel `vrp_users`
+--
+ALTER TABLE `vrp_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Begrænsninger for dumpede tabeller
+--
+
+--
+-- Begrænsninger for tabel `vrp_user_business`
+--
+ALTER TABLE `vrp_user_business`
+  ADD CONSTRAINT `fk_user_business_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+
+--
+-- Begrænsninger for tabel `vrp_user_data`
+--
+ALTER TABLE `vrp_user_data`
+  ADD CONSTRAINT `fk_user_data_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+
+--
+-- Begrænsninger for tabel `vrp_user_homes`
+--
+ALTER TABLE `vrp_user_homes`
+  ADD CONSTRAINT `fk_user_homes_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+
+--
+-- Begrænsninger for tabel `vrp_user_ids`
+--
+ALTER TABLE `vrp_user_ids`
+  ADD CONSTRAINT `fk_user_ids_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+
+--
+-- Begrænsninger for tabel `vrp_user_moneys`
+--
+ALTER TABLE `vrp_user_moneys`
+  ADD CONSTRAINT `fk_user_moneys_users` FOREIGN KEY (`user_id`) REFERENCES `vrp_users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
